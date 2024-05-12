a7dcc7308db8fbf1b39d39e944f818fd22173dd5
Gere Lajos <streetnoizenova@gmail.com>
2024-05-12 21:06:58 +0200

    feat: add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: "resources/img",
+                    dest: "",
+                },
+            ],
+        }),
+    ],
     base: "/vendor/web-tinker/",
     resolve: {
         alias: {
